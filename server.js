3193b254008538ef52b84cef9deaad6b268059ad
kirkneri <dev.kirkbugayong@gmail.com>
2023-11-19 20:33:01 -0500

    added axios and modified favicongrabber api

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const axios = require('axios'); // Adding axios for making HTTP requests
+const { URL } = require('url'); // Node.js URL module to parse URLs
 const methodOverride = require('method-override')
 const port = 8080
 const Links = require('./models/links')
 
+// Function to extract domain without protocol and "www"
+const extractDomain = (url) => {
+    const parsedUrl = new URL(url);
+    return parsedUrl.hostname.replace(/^www\./, '');
+  };
+
+//MongoDB
 mongoose.connect('mongodb+srv://devkirkbugayong:qwerty123@kodego.btziqza.mongodb.net/linksResource')
 .then(() => {
     console.log('MongoDB connection established.')
@@ -13,6 +22,7 @@ mongoose.connect('mongodb+srv://devkirkbugayong:qwerty123@kodego.btziqza.mongodb
     console.error('Error has occured', err)
 })
 
+//Middleware
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'));
@@ -25,24 +35,40 @@ app.get('/', async (req, res) => {
     res.render('links/main', { links: links })
 })
 
-//Add item
+// Add item with fetching favicon
 app.post('/', async (req, res) => {
-    const { name, link, icon } = req.body;
-    let iconURL = icon || 'https://i.postimg.cc/KvMQdmMb/OIG.jpg'
-    const newLink = new Links({
+    const { name, link } = req.body;
+    let iconURL = 'https://i.postimg.cc/KvMQdmMb/OIG.jpg'; // Default icon URL
+  
+    try {
+      const domain = extractDomain(link);
+  
+      // Fetching favicon using favicongrabber.com
+      const faviconResponse = await axios.get(`https://favicongrabber.com/api/grab/${domain}`);
+      if (faviconResponse.data.icons && faviconResponse.data.icons.length > 0) {
+        // Use the first icon found
+        iconURL = faviconResponse.data.icons[0].src;
+      }
+  
+      const newLink = new Links({
         name,
         link,
         icon: iconURL,
-    })
-
-    try {
-        await newLink.save()
-        res.redirect('/')
+      });
+  
+      await newLink.save();
+      res.redirect('/');
     } catch (error) {
-        console.error(error)
-        res.status(500).send('Error creating a new link');
+      console.error(error);
+      const newLink = new Links({
+        name,
+        link,
+        icon: iconURL,
+      });
+      await newLink.save();
+        res.redirect('/');
     }
-})
+  });
 
 //Update item
 app.get('/edit/:id', async (req, res) => {
